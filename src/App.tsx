ac3b6160a0b2689f8f77becdc2113904946977a5
PSY <sksssd234@naver.com>
2024-05-03 15:14:26 +0900

    style: Layout에 menu 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Router from 'Components/Router';
 import { Layout } from 'Components/Layout';
+import { getAuth } from 'firebase/auth';
+import { app } from 'firebaseApp';
 
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
   return (
     <Layout>
-      <Router />;
+      <Router />
     </Layout>
   );
 }
